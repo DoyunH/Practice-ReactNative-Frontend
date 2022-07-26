549bafa3a7c2c4b5174fba654a127bfa91e276fe
Doyun <doyun@talktomeinkorean.com>
2022-07-26 11:40:19 +0900

    CU-2v1duuy - dummy-data and category.ts setting

---
+++ data/dummy-data.ts
@@ -0,0 +1,7 @@
+import {Category} from '../models/category';
+
+export const CATEGORIES = [
+  new Category('1', 'JWPlayer', '#f8c44c'),
+  new Category('2', 'second', '#3ebd5c'),
+  new Category('3', 'third', '#428fff'),
+];
