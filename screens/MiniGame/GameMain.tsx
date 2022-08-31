556bc910655c6d4b4afc13df6d8e3a6db62ba0c7
Doyun <doyun@talktomeinkorean.com>
2022-08-31 17:00:35 +0900

    feature: add background Image component

--- screens/MiniGame/GameMain.tsx
+++ screens/MiniGame/GameMain.tsx
@@ -1,11 +1,17 @@
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, ImageBackground} from 'react-native';
 import React from 'react';
 import StartGameScreen from './StartGameScreen';
 
 const GameMain = () => {
   return (
     <View style={styles.container}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require('../../assets/images/game.jpg')}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
     </View>
   );
 };
@@ -16,5 +22,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'f9f9f9',
+  },
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.1,
   },
 });
